0f4e7678764e1dfde05b4e8b336a336292595ef1
Extruded <extrude57@gmail.com>
2018-05-29 11:09:24 -0700

    Styling Modal
    Over loading modal styleing
    functions

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -5,9 +5,12 @@ const OptionModal = (props) => {
         <div><p><Modal isOpen={!!props.selectedOption} 
         contentLabel={"Yonigger Modal"} 
         onRequestClose={props.clearSelectedOptions}
-        ><p>I'm a yoniggerin'</p>
-        {props.selectedOption && <p>{props.selectedOption}</p>}
-        <button onClick={props.clearSelectedOptions}>Clear</button>
+        closeTimeoutMS={200}
+        className="modal"
+        >
+        <h3 className="modal__title">Selected Option</h3>
+        {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+        <button className="button" onClick={props.clearSelectedOptions}>Clear</button>
         </Modal>helloModal</p>
         
         </div>
